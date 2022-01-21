9e5797ed831f8453871215b61fbf39b5f130c3cb
YitavGil <yitavga@yahoo.com>
2022-01-21 12:22:00 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const req = require('express/lib/request');
+const mongoose = require('mongoose');
+const User = require("./User");
+const {addUser, deleteUser, depositCash, transferCash, withdrawCash} = require('./utils')
+const cors = require('cors')
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+mongoose.connect("mongodb+srv://Yitav:eozQsdKZRA5t9ria@cluster0.ztd7z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true }).then( (e) => {
+    console.log("You are connected")
+    console.log(e);
+}
+)
+
+
+async function run() {
+ const user = await User.create({
+    name: "Chubaka",
+    city: "Kashyyyk",
+    age: 104,
+    cash: 3000000,
+    credit: 5000000 })
+    user.save()
+ }
+//  run()
+
+
+
+ app.get('/users',async (req, res) => {
+    try{
+        if(req.query.name) {
+            const users = await User.find({name:req.query.name})
+            res.status(200).json(users)
+        }
+        else{
+            const users = await User.find()
+            res.status(200).json(users)
+        }
+      
+    }
+    catch(e){
+        res.status(400).send({error: e.message})
+    }
+    
+})
+
+app.post('/users', async(req, res) => {
+    try {
+        const user = await addUser(req.body)
+        res.status(200).json(user)
+    }
+    catch(e){
+        res.status(400).send({error: e.message})
+    }
+})
+
+app.delete('/users/:id', async(req, res) => {
+    try{
+        await deleteUser(req.params.id)
+        res.status(200).send()
+    }
+    catch(e) {
+        console.log(e);
+    }
+})
+
+app.patch('/users/:id', async(req, res) => {
+    try{
+    const user = await depositCash(req.params.id, req.body.cash)
+    res.status(200).json(user)
+        }
+        catch(e){
+            console.log(e);
+        }
+})
+
+
+ const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
+
---
+++ utils.js
@@ -0,0 +1,91 @@
+const { findOne } = require("./User");
+const User = require("./User");
+
+
+const stringToJson = (message, string, message2, string2) => {
+    return JSON.stringify({ [message]: string, [message2]: string2 });
+  };
+
+  const getUserIndex =(userId) => {
+    const users = loadUsers();
+    const index = users.findIndex((user) => {
+        if (user.id === userId){
+            return true
+        }
+        else {
+            return false
+        }
+    })
+    return index;
+  }
+
+const addUser = async (body) => {
+    const userExist = await User.findOne({_id: body._id})
+    if(userExist) {
+        throw Error('The user is already exist');
+    }
+  const user = new User(body)
+  await user.save()
+    return user;
+}
+
+
+const deleteUser = async (userId) => {
+   const user = await User.deleteOne({_id: userId})
+    if(!user) {
+        throw Error('User not found');
+    }
+    // saveUsers(updatedUsers)
+}
+
+const depositCash = async (userId, cashAmount) => {
+    const user = await User.findOne({_id: userId})
+    if(!user){
+        const e = new Error('User not found')
+        e.status = 404
+        throw e;
+    }
+    
+    user.cash += cashAmount
+    await user.save()
+    return user
+}   
+
+const withdrawCash = (userId, cashAmount) => {
+    const users = loadUsers();
+    const index = getUserIndex(userId)
+    if(index === -1){
+        throw Error('User not found'); 
+    }
+    users[index].cash -= cashAmount
+    // saveUsers(users)
+}
+
+const transferCash = (userId, cashAmount, targetId) => {
+    const users = loadUsers();
+    const index = getUserIndex(userId);
+    if(index === -1){
+        throw Error('Transferring user not found'); 
+    }
+
+    const targetIndex = getUserIndex(targetId);
+    if(targetIndex === -1){
+        throw Error('Target user not found'); 
+    }
+    if( users[index].cash + users[index].credit < cashAmount ){
+        throw Error('User Does not have enough credit to make the transfer')
+    }
+    users[targetIndex].cash += cashAmount;
+    users[index].cash -= cashAmount;
+    // saveUsers(users)
+}
+
+
+
+module.exports = {
+    addUser,
+    deleteUser,
+    depositCash,
+    transferCash,
+    withdrawCash
+}
